2f9a3c4604b5db182974fca4742e84739bdba165
andressafirmino <andressafirmino78gmail.comgit remote add origin https://github.com/andressafirmino/P3---DrivenEats.gitclearcd ..lsgit config --global user.email andressafirmino78gmail.com>
2023-04-09 18:39:09 -0300

    Versão com a distribuição de cartas configurada

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
 let numeroCartas = 0;
 
-const imagens = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
+const imagens = ["bobrossparrot.gif", "bobrossparrot.gif",
+"explodyparrot.gif", "explodyparrot.gif", 
+"fiestaparrot.gif", "fiestaparrot.gif",
+"metalparrot.gif", "metalparrot.gif",
+"revertitparrot.gif", "revertitparrot.gif",
+"tripletsparrot.gif", "tripletsparrot.gif",
+"unicornparrot.gif", "unicornparrot.gif"];
 
 while (numeroCartas < 4 || numeroCartas > 14 || numeroCartas % 2 !== 0) {
     numeroCartas = prompt('Escolha o número de cartas\n- Número par\n- Número maior ou igual a 4\n- Número menor ou igual a 14');
 }
 
-function virarCartas() {
-    let virarF = document.querySelector('caixa-cartas');
-    virarF.classList.remove('front');
+function virarCartas(carta) {
+    let virarF = carta.querySelector('.front');
     virarF.classList.add('frontV');
 
-    let virarV = document.querySelector('caixa-cartas');
-    virarV.classList.remove('back');
+    let virarV = carta.querySelector('.back');
     virarV.classList.add('backV');
 
 }
+ const duplas = numeroCartas / 2;
+ let imgVerso = [];
+ let imgSortida = [];
 
+ function embaralhar () {
+    return Math.random() -0.5;
+}
 
 let list = document.querySelector('.game');
+
+    for (let i = 0; i < numeroCartas; i++) {
+        imgVerso.push(imagens[i])
+        imgVerso.sort(embaralhar);
+    }
     
 
-    for (let c=0; c < numeroCartas; c++) {
-     
-        let imgVerso = imagens[c];
-        console.log(imgVerso);
+    for (let s=0; s < numeroCartas; s++) {
+
+        imgSortida.push(imgVerso[s]);
+        console.log(imgSortida);
         list.innerHTML += `<div class="caixa-cartas" onclick= "virarCartas(this)">
         <div class="front cartas"><img src="./imagens/front 2.png" /></div>
-        <div class="back cartas"><img src="./imagens/${imgVerso[c]}" /></div>
-    </div>`;
-
+        <div class="back cartas"><img src="./imagens/${imgSortida[s]}" /></div>
+    </div>`;  
 
     }
-
-
 
 
 
