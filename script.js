16e3c67834ff49e6d54c146e840c2329e9a0a660
andressafirmino <andressafirmino78gmail.comgit remote add origin https://github.com/andressafirmino/P3---DrivenEats.gitclearcd ..lsgit config --global user.email andressafirmino78gmail.com>
2023-04-10 13:40:09 -0300

    Versão com fim de jogo

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let numeroCartas = 0;
 
-const imagens = ["bobrossparrot.gif", "bobrossparrot.gif",
-"explodyparrot.gif", "explodyparrot.gif", 
-"fiestaparrot.gif", "fiestaparrot.gif",
-"metalparrot.gif", "metalparrot.gif",
-"revertitparrot.gif", "revertitparrot.gif",
-"tripletsparrot.gif", "tripletsparrot.gif",
-"unicornparrot.gif", "unicornparrot.gif"];
+const imagens = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
 
 while (numeroCartas < 4 || numeroCartas > 14 || numeroCartas % 2 !== 0) {
     numeroCartas = prompt('Escolha o número de cartas\n- Número par\n- Número maior ou igual a 4\n- Número menor ou igual a 14');
@@ -25,11 +19,11 @@ let segunda = '';
 let primeiro = '';
 let segundo = '';
 
-let par = '';
+let par = 0;
 let impar = '';
 
-
-
+let contarVirada = 0;
+let contarJogadas = 0;
 
 
 
@@ -37,95 +31,92 @@ let impar = '';
  let imgVerso = [];
  let imgSortida = [];
 
- function embaralhar () {
-    return Math.random() -0.5;
-}
+ 
 
 let list = document.querySelector('.game');
 
-    for (let i = 0; i < numeroCartas; i++) {
-        imgVerso.push(imagens[i])
-        imgVerso.sort(embaralhar);
-    }
     
-let identificar = '';
-    for (let s=0; s < numeroCartas; s++) {
 
-        imgSortida.push(imgVerso[s]);
-        identificar = imgSortida[s];
-        list.innerHTML += `<div class="caixa-cartas" onclick = "virarCartas(this)">
+
+    for (let s=0; s < duplas; s++) {
+        
+        imgVerso[s] = `<div class="caixa-cartas" onclick = "virarCartas(this)">
         <div class="front cartas"><img src="./imagens/front 2.png" /></div>
-        <div class="back cartas"><img data-img = ${identificar} src="./imagens/${imgSortida[s]}" /></div>
+        <div class="back cartas"><img src="./imagens/${imagens[s]}" /></div>
     </div>`;  
     }
 
+    for (let i = 0; i < duplas; i++) {
+        imgSortida.push(imgVerso[i]);
+        imgSortida.push(imgVerso[i]);
+       
+    }
 
+    imgSortida.sort(function () {
+        return 0.5 - Math.random();
+    })
+
+    for (let m = 0; m < numeroCartas; m++) {
+        list.innerHTML += imgSortida[m];
+    }
     function virarCartas(carta) {
 
-        if (primeiraCarta === '') {
-
-            primF = carta.querySelector('.front');
-            primF.classList.add('frontV');
-    
-            primeiraCarta = carta.querySelector('.back');
-            primeiraCarta.classList.add('backV');
-            primeiro = primeiraCarta.querySelector('img');
+        if (carta.classList.contains('selecionado') !== true && contarVirada === 0) {
+            carta.classList.add('selecionado');
+            contarVirada++;
+            contarJogadas++;
+            primeira = carta;
+           /* primeiro = primeiraCarta.querySelector('img');
             primeira = primeiro.getAttribute('data-img');
-            console.log(primeira);
+            console.log(primeira);*/
 
         }
 
-        else if(segundaCarta === '') {
-            segF = carta.querySelector('.front');
-            segF.classList.add('frontV');
-    
-            segundaCarta = carta.querySelector('.back');
-            segundaCarta.classList.add('backV');
-            segundo = segundaCarta.querySelector('img');
+        else if(carta.classList.contains('selecionado') !== true && contarVirada === 1) {
+            carta.classList.add('selecionado');
+            contarVirada++;
+            
+            contarJogadas++;
+            segunda = carta;
+            /*segundo = segundaCarta.querySelector('img');
             segunda = segundo.getAttribute('data-img');
-            console.log(segunda);
-
-            verificarPar(carta);
+            console.log(segunda);*/
+            setTimeout(verificarPar, 1000, primeira, segunda);
     
         }
     } 
-    
-function clicar(carta) {
-    let clicar = document.querySelectorAll('.caixa-cartas');
-    clicar.addEventListener('click', verificarPar);
-}
-function desclicar (carta) {
-    let desclicar = document.querySelector('.caixa-cartas');
-    desclicar.removeEventListener('click', verificarPar);
-}
+function verificarQtde() {
+    if (par === duplas) {
+        alert(`Você ganhou em ${contarJogadas} jogadas!`);
+    }
+} 
 
-function verificarPar (carta) {
-        if (primeira === segunda) {
-            primeiraCarta.classList.add('desabilitar');
-            segundaCarta.classList.add('desabilitar');
+function verificarPar (carta1, carta2) {
+        if (carta1.querySelector('.back > img').src === carta2.querySelector('.back > img').src) {
+            par++;
+            contarVirada = 0;
+            carta1 = '';
+            carta2 = '';
+            verificarQtde();
 
-            desclicar();
-            
-            primeiraCarta = '';
-            segundaCarta = '';
         }
-        else {
+        else if (carta1.querySelector('.back > img').src !== carta2.querySelector('.back > img').src) {
 
-            setTimeout (diferente, 1000)
+           diferente();
 
 
             function diferente() {
                 
-                primeiraCarta.classList.remove('backV');
-                primF.classList.remove('frontV');
     
             
-                segundaCarta.classList.remove('backV');
-                segF.classList.remove('frontV');
+                carta1.classList.remove('selecionado');
+                carta2.classList.remove('selecionado');
         
-            
-                primeiraCarta = '';
-                segundaCarta = '';
+                contarVirada = 0;
+
+                carta1 = '';
+                carta2 = '';
+
             }
         }
     }
